e2a5102653ae988712a784a356ef69c8c19c4ba9
RichardKT88 <kendy_t88@hotmail.com>
2021-09-27 17:47:20 -0300

    End of section 9

---
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Icon, Item, Segment } from 'semantic-ui-react';
+import { Activity } from '../../../app/models/activity';
+
+interface Props {
+    activity: Activity
+}
+
+export default function ActivityListItem({activity} : Props) {
+    
+    return (
+       <Segment.Group>
+           <Segment>
+               <Item.Group>
+                   <Item>
+                       <Item.Image size="tiny" circular src='/assets/user.png' />
+                       <Item.Header as={Link} to={`/activities/${activity.id}`}>{activity.title}</Item.Header>
+                       <Item.Description>Hosted By Raj</Item.Description>
+                   </Item>
+               </Item.Group>
+           </Segment>
+           <Segment secondary>
+               <span>
+                   <Icon name='clock' /> {activity.date}
+                   <Icon name='marker'/> {activity.venue}
+               </span>
+           </Segment>
+           <Segment clearing>
+                <span>{activity.description}</span>
+                <Button
+                    as={Link}
+                    to={`/activities/${activity.id}`}
+                    color='teal'
+                    floated='right'
+                    content='View'
+                />
+           </Segment>
+       </Segment.Group>
+    )
+}
